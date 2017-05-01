39254547c0f4872c86f984cfc088bfbb9307f02f
shaquibkhan <connectshaquib@gmail.com>
2017-05-01 14:06:19 +0530

    commit
    commit

---
+++ ass7a.js
@@ -0,0 +1,10 @@
+// Function Declaration...
+function calculateArea(radius)
+{
+	// Function Definations......
+	var pi = 3.14 ;
+	var area = pi * radius * radius ;
+	console.log("When radius is " +radius+ " Area of circle = " +area);
+}
+// Function is called .......
+calculateArea(7);
